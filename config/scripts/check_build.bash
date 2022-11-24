7b70e189a10f9366ce84340ed3dbde77d1945568
RolfNoot <smits@wth.nl>
2022-11-24 15:23:02 +0100

    Minor fixes

--- config/scripts/check_build.bash
+++ config/scripts/check_build.bash
@@ -1,15 +1,4 @@
 #!/bin/bash
-make -f CMakeFiles/$1.elf.dir/build.make CMakeFiles/$1.elf.dir/build -q
-if [ $? == 0 ] 
-then
-    printf "Build is OK :-)\n"
-    exit 0
-fi
-if [ $? == 1 ] 
-then
-    printf "**** Please build project first. ****\n\n"
-    exit 1
-else
-    printf "**** Please fix build errors. ****\n\n"
-fi
+# Checking build is depreciated
+printf "***********************************************************************************************************************\n**** ERROR: PLEASE UPDATE launch.json.                                                                             ****\n**** SEE: https://github.com/onethinx/Onethinx_Project_Examples/blob/master/Example_HelloWorld/.vscode/tasks.json *****\n***********************************************************************************************************************\n"
 exit 2
